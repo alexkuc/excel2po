0c3c6d5efc4f6a1cb9d1172933c0b10b3b9e2658
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2022-02-02 09:47:23 +0200

    Create command excel2po

---
+++ app/Console/Commands/excel2po.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class excel2po extends Command
+{
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'excel2po';
+
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Generate .po and .mo files from Excel file';
+
+  /**
+   * Create a new command instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  /**
+   * Execute the console command.
+   *
+   * @return int
+   */
+  public function handle()
+  {
+    return 0;
+  }
+}
---
+++ tests/Feature/Console/Commands/excel2poTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tests\Feature\Console\Commands;
+
+use Tests\TestCase;
+
+class excel2poTest extends TestCase
+{
+}
